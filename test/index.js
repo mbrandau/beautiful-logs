2c9e924acea764a20ad344762ae8b9204d450f6a
Maximilian Brandau <mbrandau@online.de>
2017-09-16 16:13:57 +0200

    Remove tests of the logging functions

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ describe('beautiful-logs', function() {
   describe('logger', function() {
     describe('.addCallback()', function() {
       it('should be a function', () => expect(init().addCallback).to.be.a('function'));
-      it('should be called on log events', function(done) {
+      it('added callbacks should be called on log events', function(done) {
         const logger = init();
         logger.addCallback(function(event) {
           expect(event).to.have.property('level', 'info');
@@ -16,53 +16,6 @@ describe('beautiful-logs', function() {
           done();
         });
         logger.info('Test message!');
-      });
-    });
-    describe('.emptyLine()', function() {
-      it('should print a empty line on the console', function(done) {
-        const oldFn = console.log;
-        console.log = function() {
-          expect(arguments.length).to.be(0);
-          console.log = oldFn;
-          done();
-        }
-        init().emptyLine();
-      });
-    });
-    describe('.log()', function(done) {
-      const logger = init();
-      it('should return a string', function() {
-        expect(logger.log('info', 'Test')).to.be.a('string');
-      });
-    });
-    describe('.trace()', function() {
-      it('should return a string', function() {
-        expect(init().trace('Test')).to.be.a('string');
-      });
-    });
-    describe('.debug()', function() {
-      it('should return a string', function() {
-        expect(init().debug('Test')).to.be.a('string');
-      });
-    });
-    describe('.info()', function() {
-      it('should return a string', function() {
-        expect(init().info('Test')).to.be.a('string');
-      });
-    });
-    describe('.warn()', function() {
-      it('should return a string', function() {
-        expect(init().warn('Test')).to.be.a('string');
-      });
-    });
-    describe('.err()', function() {
-      it('should return a string', function() {
-        expect(init().err('Test')).to.be.a('string');
-      });
-    });
-    describe('.fatal()', function() {
-      it('should return a string', function() {
-        expect(init().fatal('Test')).to.be.a('string');
       });
     });
   });
